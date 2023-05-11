8eefcb8fcf91d8e00814641cb0dd261a021514b6
Andrei Krotov <akrotoff95@gmail.com>
2023-05-11 15:30:24 +0300

    Added CoordinatableViewController

--- Sources/CoordinatableViewController/CoordinatableViewController.swift
+++ Sources/CoordinatableViewController/CoordinatableViewController.swift
@@ -1,6 +1,29 @@
-public struct CoordinatableViewController {
-    public private(set) var text = "Hello, World!"
+import UIKit.UIViewController
 
-    public init() {
+open class CoordinatableViewController: UIViewController {
+
+    // MARK: - Public properties
+
+    public var onCompleted: (() -> Void)?
+
+    // MARK: - View lifecycle
+
+    public override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        if let navigationController = navigationController {
+            if !navigationController.viewControllers.contains(self) {
+                onCompleted?()
+            }
+        } else if isParentless {
+            onCompleted?()
+        }
+    }
+}
+
+public extension UIViewController {
+
+    var isParentless: Bool {
+        parent == nil && presentingViewController == nil && UIApplication.shared.keyWindow?.rootViewController != self
     }
 }
